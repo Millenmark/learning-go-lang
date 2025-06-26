8d809ec61b03d81df922d72702fc45c3397949fb
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-06-26 20:59:06 +0800

    file creation in go

---
+++ reading/Beginner - Mastering Go Lang/getting-started.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+// func main() {
+// 	myfile, es := os.Create("helloo.txt")
+// 	if es != nil {
+// 		log.Fatal(es)
+// 	}
+
+// 	log.Println(myfile)
+// 	myfile.Close()
+// }
+
+func main() {
+	fileName := "hellow.txt"
+	myfile, es := os.Stat(fileName)
+	if es != nil {
+		if os.IsNotExist(es) {
+			// log.Fatal("File not found")
+			os.Create(fileName)
+			log.Println("File not found so I created it for you.")
+		} else {log.Fatal(es)}
+	}
+	log.Println("File Exist")
+	log.Println("File Detail is:")
+	log.Println("Name is: ", myfile.Name())
+	log.Println("Size is: ", myfile.Size())
+}
