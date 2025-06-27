d8dd902e5acb3a4048600b97708680c5dfb693ad
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-06-27 21:29:21 +0800

    making directory

--- reading/Beginner - Mastering Go Lang/getting-started.go
+++ reading/Beginner - Mastering Go Lang/getting-started.go
@@ -16,17 +16,21 @@ import (
 // }
 
 func main() {
-	fileName := "hellow.txt"
-	myfile, es := os.Stat(fileName)
-	if es != nil {
-		if os.IsNotExist(es) {
-			// log.Fatal("File not found")
-			os.Create(fileName)
-			log.Println("File not found so I created it for you.")
-		} else {log.Fatal(es)}
+	// fileName := "hellow.txt"
+	// myfile, es := os.Stat(fileName)
+	// if es != nil {
+	// 	if os.IsNotExist(es) {
+	// 		// log.Fatal("File not found")
+	// 		os.Create(fileName)
+	// 		log.Println("File not found so I created it for you.")
+	// 	} else {log.Fatal(es)}
+	// }
+	// log.Println("File Exist")
+	// log.Println("File Detail is:")
+	// log.Println("Name is: ", myfile.Name())
+	// log.Println("Size is: ", myfile.Size())
+
+	if er := os.Mkdir("a", os.ModePerm) ; er != nil {
+		log.Fatal(er)
 	}
-	log.Println("File Exist")
-	log.Println("File Detail is:")
-	log.Println("Name is: ", myfile.Name())
-	log.Println("Size is: ", myfile.Size())
 }
