5ad3376c515b00de8d7782360a6ceae3aafd58aa
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-07-02 13:41:27 +0800

    print formats with struct

--- reading/goCrazy/try.go
+++ reading/goCrazy/try.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
+type Message struct {
+	Hello string
+}
+
 func main() {
-	fmt.Printf("Go version: %s\n", runtime.Version())
+	// fmt.Printf("Go version: %s\n", runtime.Version())
+	h := Message{Hello: "WORLD"}
+	fmt.Printf("%s\n", h)
+	fmt.Printf("%+v\n", h)
 }
