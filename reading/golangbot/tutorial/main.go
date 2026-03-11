00125df83302028aea831db435f37e84b8bef13e
Millenmark <millenmark.dev@gmail.com>
2026-03-11 15:03:07 +0800

    new

---
+++ reading/golangbot/tutorial/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 124.45, 3.45
+	fmt.Println(a, b)
+}
--- udemy/josephabah/main.go
+++ udemy/josephabah/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+	a, b := 124.45, 3.45
 	fmt.Println("Hello madafackeer")
 	fmt.Println("%+v\n", []int{1, 2, 3})
+	fmt.Println(math.Min(a, b))
 }
