308be70ac1ca4318358f35e26669d533c337ea4a
Millenmark <millenmark.dev@gmail.com>
2026-03-13 10:54:39 +0800

    uint data type

--- reading/golangbot/datatypes/datatypes.go
+++ reading/golangbot/datatypes/datatypes.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var a int = 89
+	b := 95
+	fmt.Println("value of a is", a, "and b is", b)
+	fmt.Printf("type of a is %T, size of a is %d bytes\n", a, unsafe.Sizeof(a))
+	fmt.Printf("type of b is %T, size of b is %d bytes\n", b, unsafe.Sizeof(b))
+
+	var c uint = 56
+	var d uint = 78
+	e := c * d
+	fmt.Printf("type of e is %T, size of e is %d bytes\n", e, unsafe.Sizeof(e))
+
 }
