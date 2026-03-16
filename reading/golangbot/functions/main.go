a07a53ab0b2da332caf1b1037c837914d3fe2be4
Millenmark <millenmark.dev@gmail.com>
2026-03-16 13:14:40 +0800

    anonymous functions

--- reading/golangbot/functions/main.go
+++ reading/golangbot/functions/main.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	taxRate := 0.2
+
 	result := calculateBill(23, 2)
 	fmt.Println(result)
+
+	// closures
+	calculateTax := func(price float64) float64 {
+		return price * taxRate
+	}
+
+	tax := calculateTax(float64(result))
+	fmt.Println(tax)
 }
