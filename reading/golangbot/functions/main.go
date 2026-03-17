6bbdff78443ad923cecffd23aa3d241dc82df255
Millenmark <millenmark.dev@gmail.com>
2026-03-17 10:12:25 +0800

    multiple return values of a function

--- reading/golangbot/functions/main.go
+++ reading/golangbot/functions/main.go
@@ -8,11 +8,17 @@ func main() {
 	result := calculateBill(23, 2)
 	fmt.Println(result)
 
-	// closures
+	// closures (anonymous function)
 	calculateTax := func(price float64) float64 {
 		return price * taxRate
 	}
 
 	tax := calculateTax(float64(result))
 	fmt.Println(tax)
+
+	rectProps := func(width, height float64) (float64, float64) {
+		return width * height, width + height
+	}
+
+	fmt.Println(rectProps(4, 5))
 }
